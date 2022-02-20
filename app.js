d29fa8f66c39f8849b7ec84d34bad0862d95ce40
shikou0918 <shikou.0918@gmail.com>
2022-02-20 21:02:09 +0900

    js-practice

---
+++ app.js
@@ -0,0 +1,10 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？'
+const answers = [
+  'スーパーファミコン',
+  'プレイステーション',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+
+document.getElementById('js-question').textContent
